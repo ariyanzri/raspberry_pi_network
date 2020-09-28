25b83f58259de93230467c3bf259cbd469f0f355
ariyanzri <ariyanzarei@email.arizona.edu>
2020-09-27 23:36:56 -0700

    update

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
+import re
 
 f = os.popen('sudo nmap -sP 192.168.0.*')
 output = f.read()
 
-lines = output.split('\nNmap')
+connected_devices = output.split('\nNmap ')
 
-for l in lines:
-	print(l)
-	print('------')
+for d in connected_devices:
+# 	scan report for 192.168.0.5
+# Host is up (0.048s latency).
+# MAC Address: B8:BC:5B:94:96:F1 (Unknown)
+
+	m = re.match("\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b",d)
+	print(m.group(0))
