e1451df2dc6a4bc2ea4e1fee5dfccaa2b7e33a14
ariyanzri <ariyanzarei@email.arizona.edu>
2020-09-28 22:02:50 -0700

    devices get

--- main.py
+++ main.py
@@ -1,28 +1,69 @@
 import os
+import time 
 
-f = os.popen('sudo nmap -sP 192.168.0.*')
-output = f.read()
+class Device:
 
-connected_devices = output.split('\nNmap ')
+	def __init__(self,name,ip,mac):
+		self.name = name
+		self.ip_address = ip
+		self.mac_address = mac
 
-for d in connected_devices:
+	def __eq__(self,obj):
+		if obj.mac_address == self.mac_address:
+			return True
+		return False
 
-	ip_address = ""
-	mac_address = ""
-	name = ""
+	def __str__(self):
+		return "--- device name: ({0}) - IP address: {1} - MAC address: {2} ---".format(self.name,self.ip_address,self.mac_address)
 
-	lines = d.split('\n')
+def get_list_devices():
 
-	for l in lines:
-		if "scan report for" in l:
-			ip_address = l.split("for ")[1]
+	device_list = []
 
-		elif "MAC Address" in l:
-			temp = l.split("Address: ")[1]
-			mac_address = temp.split(" ")[0]
-			name = temp.split(" ")[1].replace('(','').replace(')','')
+	f = os.popen('sudo nmap -sP 192.168.0.*')
+	output = f.read()
 
-	if ip_address == "" and mac_address == "" and name == "":
-		continue
+	connected_devices = output.split('\nNmap ')
 
-	print("Device Name: ({0}) - Device IP: ({1}) - Device MAC: ({2})".format(name,ip_address,mac_address))
+	for d in connected_devices:
+
+		ip_address = ""
+		mac_address = ""
+		name = ""
+
+		lines = d.split('\n')
+
+		for l in lines:
+			if "scan report for" in l:
+				ip_address = l.split("for ")[1]
+
+			elif "MAC Address" in l:
+				temp = l.split("Address: ")[1]
+				mac_address = temp.split(" ")[0]
+				name = temp.split(" ")[1].replace('(','').replace(')','')
+
+		if ip_address == "" and mac_address == "" and name == "":
+			continue
+
+		dvc = Device(name,ip_address,mac_address)
+		device_list.append(dvc)
+
+
+update_interval = 10
+current_devices = []
+
+while True:
+	devices = get_list_devices()
+
+	for d in devices:
+		if d not in current_devices:
+			print("Device connected: {0}".format(d))
+
+	for d in current_devices:
+		if d not in devices:
+			print("Device disconnected: {0}".format(d))
+
+	current_devices = devices
+
+	time.sleep(20)
+
